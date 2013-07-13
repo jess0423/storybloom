b518bf71225ce508a1bcaf443f0ad22a97d271fe
Jessica Blaisdell <jessheuv@gmail.com>
2013-07-12 21:28:09 -0500

    Index page

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from blogs import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+     url(r'/photo$', views.index, name='index')
+    
 )
